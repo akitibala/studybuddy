944f0738d616be8eaac8ac21c214f2216aae802f
akitibala <akiti.bala.murali.krishna.reddy@hp.com>
2021-10-20 17:45:43 +0530

    delete room operation added

--- base/urls.py
+++ base/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('',view=views.home,name='home'),
     path('room/<str:pk>',view=views.room,name='room'),
     path('create-room',view=views.create_room,name='create-room'),
-    path('update-room/<str:pk>',view=views.update_room,name='update-room')
+    path('update-room/<str:pk>',view=views.update_room,name='update-room'),
+    path('delete-room/<str:pk>',view=views.delete_room,name='delete-room'),
 ]
