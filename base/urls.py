bd84da88531044e212205e2e0268ea40bbef88b2
akitibala <akiti.bala.murali.krishna.reddy@hp.com>
2021-10-20 16:42:05 +0530

    update room operation added

--- base/urls.py
+++ base/urls.py
@@ -4,5 +4,7 @@ from . import views
 
 urlpatterns = [
     path('',view=views.home,name='home'),
-    path('room/<str:pk>',view=views.room,name='room')
+    path('room/<str:pk>',view=views.room,name='room'),
+    path('create-room',view=views.create_room,name='create-room'),
+    path('update-room/<str:pk>',view=views.update_room,name='update-room')
 ]
